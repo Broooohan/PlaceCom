1083f738360a4526cca21a06522a3649b45d87e5
Rohan <2020kuec2064@iiitkota.ac.in>
2023-08-12 21:24:00 +0530

    tryout

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(require('./router/auth')); //middleware
 
 app.use(express.static(path.join(__dirname,"./client/build")));
 
-app.get('*', function (req,res) {
+app.get('/*', function (req,res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
